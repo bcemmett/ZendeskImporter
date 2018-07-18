f4e3f5bb8409f43ed53a4cb9d7b7fe3cbfc381d7
Ben Emmett <bcemmett@users.noreply.github.com>
2018-07-19 00:46:56 +0100

    Tables for user data

---
+++ Database/Tables/dbo.Users.sql
@@ -0,0 +1,29 @@
+CREATE TABLE [dbo].[Users]
+(
+[Active] [bit] NULL,
+[Alias] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[CreatedAt] [datetime] NULL,
+[CustomRoleId] [bigint] NULL,
+[Details] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[Email] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[ExternalId] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[Id] [bigint] NOT NULL,
+[LastLoginAt] [datetime] NULL,
+[LocaleId] [bigint] NULL,
+[Moderator] [bit] NULL,
+[Name] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[Notes] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[OnlyPrivateComments] [bit] NULL,
+[OrganizationId] [bigint] NULL,
+[Phone] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[RemotePhotoUrl] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[Role] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[Shared] [bit] NULL,
+[Signature] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[Suspended] [bit] NULL,
+[TicketRestriction] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[TimeZone] [nvarchar] (max) COLLATE Latin1_General_CI_AS NULL,
+[UpdatedAt] [datetime] NULL,
+[Verified] [bit] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
