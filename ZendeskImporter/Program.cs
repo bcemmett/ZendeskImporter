7e4053c7e30d85318ff306499c8a35d53e811e31
Ben Emmett <bcemmett@users.noreply.github.com>
2018-07-23 19:48:54 +0100

    Get zendisk config data from app.config

--- ZendeskImporter/Program.cs
+++ ZendeskImporter/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace ZendeskImporter
+﻿using System.Configuration;
+
+namespace ZendeskImporter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var runner = new ImportRunner("XXXXX", "YYYYY", "ZZZZZ");
+            var runner = new ImportRunner(
+                ConfigurationManager.AppSettings["ZendeskUrl"],
+                ConfigurationManager.AppSettings["ZendeskUser"],
+                ConfigurationManager.AppSettings["ZendeskToken"]);
+
             runner.Run();
         }
     }
