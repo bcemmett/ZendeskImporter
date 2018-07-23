55131e1591d476e8caa873d9161b50df732aa04e
Ben Emmett <bcemmett@users.noreply.github.com>
2018-07-23 19:06:18 +0100

    Remove error handling around ticket import

--- ZendeskImporter/Program.cs
+++ ZendeskImporter/Program.cs
@@ -14,32 +14,18 @@ namespace ZendeskImporter
             var tickets = api.GetAllTickets();
             int totalTickets = tickets.Count;
             int currrentTicket = 0;
-            int failures = 0;
+
             foreach (var ticket in tickets)
             {
-                if (failures >= 50)
+                Console.WriteLine($"Processing ticket {ticket.Id.Value} ({currrentTicket++} of {totalTickets})");
+                Thread.Sleep(250);
+                if (ticket.Status != "deleted")
+                persister.SaveTicket(ticket, ticketCustomFieldLookup);
                 {
-                    Console.WriteLine("Too many failures :-(");
-                    Console.ReadLine();
-                }
-                try
-                {
-                    Console.WriteLine($"Processing ticket {ticket.Id.Value} ({currrentTicket++} of {totalTickets})");
-                    Thread.Sleep(250);
-                    if (ticket.Status != "deleted")
-                    persister.SaveTicket(ticket, ticketCustomFieldLookup);
-                    {
-                        var comments = api.GetTicketComments(ticket.Id.Value);
-                        persister.SaveTicketComments(ticket.Id.Value, comments);
-                        var metrics = api.GetTicketMetrics(ticket.Id.Value);
-                        persister.SaveTicketMetrics(ticket.Id.Value, metrics);
-                    }
-                    failures = 0;
-                }
-                catch (Exception e)
-                {
-                    failures++;
-                    LogWriter.Append(ticket.Id.Value);
+                    var comments = api.GetTicketComments(ticket.Id.Value);
+                    persister.SaveTicketComments(ticket.Id.Value, comments);
+                    var metrics = api.GetTicketMetrics(ticket.Id.Value);
+                    persister.SaveTicketMetrics(ticket.Id.Value, metrics);
                 }
             }
 
